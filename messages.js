7baf0928b4fbaf0c4925ad3c1756d4761d9ede06
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-11 23:35:34 +0600

    Create messages.js

---
+++ messages.js
@@ -0,0 +1,13 @@
+export const welcomeMessages = [
+  "👋 Welcome {name} to our group!",
+  "🔥 Hey {name}, glad you joined!",
+  "💙 Welcome {name}, stay active!",
+  "🚀 {name} joined the community!",
+  "🌟 Welcome onboard {name}!",
+  "🎉 Hey {name}, enjoy your stay!",
+  "💬 Hello {name}, welcome here!",
+  "🤝 Nice to meet you {name}!",
+  "✨ Welcome {name} family member!",
+  "📢 {name} just joined us!"
+  // এভাবে 30টা পর্যন্ত বাড়াবে
+];
