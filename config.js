228b8612678b6ccafe16ade2953c5ea2cf31e5e8
smartmethodowner904 <quotexrealid9045@gmail.com>
2026-05-11 23:34:44 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+export const BOT_TOKEN = "8254268214:AAHoKvEQb48kwNZN_Dh-rpRqlxlgh-WmYbQ";
+
+export const GROUPS = [
+  "-1002346718545",
+  "-1003527248014"
+];
+
+export const CHANNEL_ID = "-1002315458574";
+export const METHOD_CHANNEL = "-1002510081290";
+
+export const ADMIN_ID = 8136997138;
+
+export const LINK_EXPIRE = 3600; // 1 hour
+export const MEMBER_LIMIT = 1;
